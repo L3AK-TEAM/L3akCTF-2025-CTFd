d8e7aafc09446f65becb7e42dc9dd65b9172ccfa
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2025-06-22 21:15:11 -0700

    feat: updated terraform for deployment

--- push-image.sh
+++ push-image.sh
@@ -4,11 +4,9 @@ set -euo pipefail
 IMAGE_NAME="ctfd"
 PROJECT_ID=$(gcloud config get-value project 2>/dev/null)
 TAG=$(git rev-parse --short HEAD)
-GCR_IMAGE="gcr.io/$PROJECT_ID/$IMAGE_NAME:$TAG"
 GCR_IMAGE_LATEST="gcr.io/$PROJECT_ID/$IMAGE_NAME:latest"
 
-echo "[+] Building and pushing Docker image for linux/amd64: $GCR_IMAGE and $GCR_IMAGE_LATEST"
-docker buildx build --platform linux/amd64 -t "$GCR_IMAGE" -t "$GCR_IMAGE_LATEST" --push .
+echo "[+] Building and pushing Docker image for linux/amd64: $GCR_IMAGE_LATEST"
+docker buildx build --platform linux/amd64 -t "$GCR_IMAGE_LATEST" --push .
 
-echo "[+] Images pushed: $GCR_IMAGE and $GCR_IMAGE_LATEST"
-echo "$GCR_IMAGE"
+echo "[+] Images pushed: $GCR_IMAGE_LATEST"
